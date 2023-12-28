409dfcffae0758f59496771318fdb770759366fc
guzman <guzman@DESKTOP-0FH4UOB>
2023-12-28 22:34:49 +0100

    Created file TreeStruct that defines class Node in order to implement Tree structures independently of Category class

---
+++ src/simple_invoicing/domain/TreeStruct.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+
+from typing import Optional, Self
+
+# why we need Self and not just Node? Because Node is not a final class,
+# and it's created with the aim of inheriting from it.
+# See PEP673 and https://stackoverflow.com/questions/72174409/type-hinting-the-return-value-of-a-class-method-that-returns-self
+
+
+class Node:
+    def __init__(self, name: str, parent: Optional[Self] = None):
+        self._name = name
+        self._children: set[Self] = set()
+        self._parent = parent
+        if self._parent:
+            self._parent._children.add(self)
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def root(self) -> Self:
+        if not self._parent:
+            return self
+        else:
+            return self._parent.root
+
+    @property
+    def parent(self) -> Optional[Self]:
+        return self._parent
+
+    @property
+    def leaves(self) -> set[Self]:
+        """Returns a set of all the leaves of the tree rooted at this node"""
+        if not self._children:
+            return set([self])
+        else:
+            leaves = set()
+            for child in self._children:
+                leaves.update(child.leaves)
+            return leaves
+
+    def is_internal(self) -> bool:
+        """Returns True if the node is not a leaf"""
+        return bool(self._children)
+
+    def delete(self) -> None:
+        """Deletes the node and all its children"""
+        if self._parent:
+            self._parent._children.remove(self)
+        children = list(self._children)
+        for subcategory in children:
+            self._children.remove(subcategory)
+        del self
+
+    def __iter__(self):
+        return iter(self._children)
+
+    def __contains__(self, item: Self) -> bool:
+        return item in self._children
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(
+            other, Node
+        ):  # tricky point. Self is for type annotation, but we need to check if other iherits from Node
+            return False
+        return (
+            self._name == other._name
+            and self._parent == other._parent
+            and self._children == other._children
+        )
+
+    def __hash__(self) -> int:
+        if self._parent:
+            return hash(self._parent.name + "." + self._name)
+        else:
+            return hash(self._name)
+
+    def __repr__(self) -> str:
+        if self._parent:
+            return f"{self.__class__.__name__}(name={self._name}, parent={self._parent.name}, children={len(self._children)})"
+        else:
+            return f"{self.__class__.__name__}(name={self._name}), children={len(self._children)})"
+
+    def __str__(self, level=0):
+        """Returns a string representation of the tree"""
+        ret = "  " * level + "|---" * (level > 0) + self._name + "\n"
+        for child in self._children:
+            ret += child.__str__(level + 1)
+        return ret
---
+++ src/simple_invoicing/domain/test_tree.py
@@ -0,0 +1,79 @@
+from TreeStruct import Node
+import pytest
+
+@pytest.fixture
+def tree():
+    '''
+                 1
+                / \
+               2   3
+              /     \
+             4       5  
+    '''
+    n1 = Node("1")
+    n2 = Node("2", parent=n1)
+    n3 = Node("3", parent=n1)
+    n4 = Node("4", parent=n2)
+    n5 = Node("5", parent=n3)
+
+    return (n1, n2, n3, n4, n5)
+
+
+
+def test_child_assignation_to_node(tree):
+    n1, n2, n3, n4, n5 = tree
+
+    assert n2 in n1
+    assert n3 in n1
+    assert n4 not in n1
+    assert n5 not in n1
+    assert n4 in n2
+    assert n5 in n3
+    assert n5 not in n2
+
+def test_leaves_property(tree):
+    n1, n2, n3, n4, n5 = tree
+
+    assert n2 not in n1.leaves
+    assert n3 not in n1.leaves
+    assert n4 in n1.leaves
+    assert n5 in n1.leaves
+    assert n4 in n2.leaves
+    assert n5 in n3.leaves
+
+
+def test_get_root_and_parent_of_nodes(tree):
+    n1, n2, _, n4, _ = tree
+
+    assert n1 == n4.root
+    assert n1 == n2.parent
+    assert n1.parent is None
+    assert n1.is_internal()
+    assert n2.is_internal()
+    assert not n4.is_internal()
+
+
+def test_node_deletion_given_one_child(tree):
+    n1, n2, n3, n4, n5 = tree
+
+    assert n1.leaves == set([n4, n5])
+
+    n3.delete()
+
+    assert n1.leaves == set([n4])
+
+
+def test_node_assignation_is_idempotent(tree):
+    n1, n2, n3, n4, n5 = tree
+
+    assert n4 in n1.leaves
+    assert len(n1.leaves) == 2
+
+    child4 = Node("4", parent=n2)
+
+    assert n4 in n1.leaves
+    assert child4 in n1.leaves
+    assert len(n1.leaves) == 2
+    assert n4 == child4
+    assert n4 is not child4
+    assert n1.leaves == set([n4, n5])
