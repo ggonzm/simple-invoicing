2f70eb4fd5e5a75df63ff1ed0e7e721ed805d858
ggonzm <108734919+ggonzm@users.noreply.github.com>
2024-01-22 07:04:46 -0500

    Class UnitOfWork converted into an AbstractClass rather than a protocol, in order to make uow's interface more generic via "repo" attribute. uow tests modified in order to reflect this new generalization over owned repository.

--- src/simple_invoicing/services/units_of_work.py
+++ src/simple_invoicing/services/units_of_work.py
@@ -2,34 +2,36 @@ from __future__ import annotations
 
 from src.simple_invoicing.persistence.repositories import Repository, FamilyRepository
 from src.simple_invoicing.persistence.db import default_conn_factory
-from typing import Protocol, Callable
+from src.simple_invoicing.domain.model import Family
+from typing import Callable
 import sqlite3
 
-class UnitOfWork[T](Protocol):
+class UnitOfWork[T]():
+    repo: Repository[T]
 
     def __enter__(self) -> UnitOfWork[T]:
-        ...
+        raise NotImplementedError
 
     def commit(self) -> None:
-        ...
+        raise NotImplementedError
 
     def rollback(self) -> None:
-        ...
+        raise NotImplementedError
     
     def __exit__(self, exc_type, exc_value, traceback) -> None:
-        ...
+        raise NotImplementedError
 
 
-class FamilyUnitOfWork():
+class FamilyUnitOfWork(UnitOfWork[Family]):
     def __init__(self, conn_factory: Callable[[], sqlite3.Connection] = default_conn_factory) -> None:
         self.conn = conn_factory()
-        self.families = FamilyRepository(self.conn)
+        self.repo = FamilyRepository(self.conn)
     
     def __enter__(self) -> FamilyUnitOfWork:
         return self
 
     def commit(self) -> None:
-        self.families.update()
+        self.repo.update()
         self.conn.commit()
     
     def rollback(self) -> None:
--- tests/integration/test_units_of_work.py
+++ tests/integration/test_units_of_work.py
@@ -5,39 +5,39 @@ def test_uow_can_add_a_family_retrieve_it_and_add_or_remove_products_using_diffe
     product1, product2, _ = fruit_trees
 
     with FamilyUnitOfWork(conn_factory) as uow:
-        assert uow.families.list() == []
+        assert uow.repo.list() == []
         family1.add(rootstock)
         family1.add(product1)
-        uow.families.add(family1)
+        uow.repo.add(family1)
         uow.commit()
 
     with FamilyUnitOfWork(conn_factory) as uow:
-        assert uow.families.get("manzanos") == family1
-        assert uow.families.get("manzanos").fruit_trees == frozenset([product1])
-        assert rootstock in uow.families.get("manzanos").rootstocks
+        assert uow.repo.get("manzanos") == family1
+        assert uow.repo.get("manzanos").fruit_trees == frozenset([product1])
+        assert rootstock in uow.repo.get("manzanos").rootstocks
 
-        family = uow.families.get("manzanos")
+        family = uow.repo.get("manzanos")
         family.add(product2)
         uow.commit()
 
     with FamilyUnitOfWork(conn_factory) as uow:
-        assert uow.families.get("manzanos").fruit_trees == frozenset([product1, product2])
+        assert uow.repo.get("manzanos").fruit_trees == frozenset([product1, product2])
     
     with FamilyUnitOfWork(conn_factory) as uow:
-        family = uow.families.get("manzanos")
+        family = uow.repo.get("manzanos")
         family.remove(product2)
         uow.commit()
     
     with FamilyUnitOfWork(conn_factory) as uow:
-        assert uow.families.get("manzanos").fruit_trees == frozenset([product1])
+        assert uow.repo.get("manzanos").fruit_trees == frozenset([product1])
     
     with FamilyUnitOfWork(conn_factory) as uow:
-        family = uow.families.get("manzanos")
+        family = uow.repo.get("manzanos")
         family.remove(rootstock)
         uow.commit()
     
     with FamilyUnitOfWork(conn_factory) as uow:
-        family = uow.families.get("manzanos")
+        family = uow.repo.get("manzanos")
         assert rootstock not in family.rootstocks
         assert family.fruit_trees == frozenset([])
     
